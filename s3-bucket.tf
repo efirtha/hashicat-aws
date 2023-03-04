40b6ccb9a0072b643f81273ad0f331ed8ca4c734
efirtha <126624350+efirtha@users.noreply.github.com>
2023-03-03 18:26:24 -0700

    Update s3-bucket.tf
    omg please make it stop

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/Firtha-lab/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "Firtha"
-
-  versioning = {
-    enabled = true
-  }
-
 }
